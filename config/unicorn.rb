5df3175aea0d69b901d2818665e80a7745909058
aliirz <dejavu87@gmail.com>
2013-08-08 12:52:43 +0500

    edited unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,6 +4,6 @@ pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 preload_app true
-listen "/tmp/unicorn.blog.sock"
+listen "/tmp/unicorn.sps.sock"
 worker_processes 2
 timeout 30

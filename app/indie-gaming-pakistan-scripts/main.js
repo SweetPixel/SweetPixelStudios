0cef8afb43d8fd9ef08ac27f01cc648ef9af074a
Muhammad Sayyam <sayyam@sweetpixelstudios.com>
2014-11-07 15:19:55 +0500

    checking diff settings

--- app/indie-gaming-pakistan-scripts/main.js
+++ app/indie-gaming-pakistan-scripts/main.js
@@ -1,7 +1,7 @@
 
 
   $(".main").onepage_scroll({
-    sectionContainer: "section",     // sectionContainer accepts any kind of selector in case you don't want to use section
+    sectionContainer: "cat",     // sectionContainer accepts any kind of selector in case you don't want to use section
     easing: "cubic-bezier(0.175, 0.885, 0.420, 1.310)",                  // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in",
     // "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
     animationTime: 1000,             // AnimationTime let you define how long each section takes to animate

98d586fa61c4fe957ebc15ea1e30d5e8f2409036
aliirz <dejavu87@gmail.com>
2015-03-06 16:54:29 +0500

    Emails now being sent, validation added

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -2,27 +2,38 @@
 
 $('#contactUs').click( function (e) {
 	e.preventDefault();
-	$.ajax({
-		type: 'POST',
-		url: 'https://mandrillapp.com/api/1.0/messages/send.json',
-		data: {
-			'key': '7MI_ClfWxAxYrHzeomxkEA',
-			'message': {
-				'from_email': 'ali@sweetpixelstudios.com',
-				'to': [
-					{
-						'email': 'hello@sweetpixelstudios.com',
-						'name': 'Hello SPS',
-						'type': 'to'
-					}
-				],
-				'autotext': 'true',
-				'subject': 'SUBJECT To change!',
-				'html': 'YOUR EMAIL CONTENT HERE! YOU CAN USE HTML!'
+	var name = $('#name').val(),
+	email = $('#email').val(),
+	message = $('#message').val();
+	if (name === '' || email === '' || message === '') {
+		sweetAlert("Oops...", "You must fill all fields!", "error");
+	}
+	else {
+		$.ajax({
+			type: 'POST',
+			url: 'https://mandrillapp.com/api/1.0/messages/send.json',
+			data: {
+				'key': '7MI_ClfWxAxYrHzeomxkEA',
+				'message': {
+					'from_email': 'ali@sweetpixelstudios.com',
+					'to': [
+						{
+							'email': 'usman@sweetpixelstudios.com',
+							'name': 'Usman SPS',
+							'type': 'to'
+						}
+					],
+					'autotext': 'true',
+					'subject': 'New Feedback from SPS Website!',
+					'html': '<strong>From: </strong>' + name + '<br />' +
+					'<strong>Email: </strong>' + email + '<br />' +
+					'<strong>Message: </strong>' + message
+				}
 			}
-		}
-	});
-	// .done(function(response) {
-	// 	console.log(response); // if you're into that sorta th
-	// });
+		}).done(function() {
+			// console.log(response); // if you're into that sorta th
+			$('#contactForm').hide();
+			$('#sentConfirmation').show();
+		});
+	}
 });
